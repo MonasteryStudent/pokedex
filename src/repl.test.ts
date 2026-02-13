586f5b506525c2b46b1c6830a351ec5114c554ec
MonasteryStudent <cs_knoll@proton.me>
2026-02-13 13:48:09 +0100

    test: add unit tests for cleanInput function (the red phase of TDD)

---
+++ src/repl.test.ts
@@ -0,0 +1,34 @@
+import {cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+
+// create a test suite
+describe.each([
+    {
+        input: " hello world ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: "Homo sine pecunia est imago mortis.",
+        expected: ["homo", "sine", "pecunia", "est", "imago", "mortis."],
+    },
+    {
+        input: "",
+        expected: [""],
+    },
+    {
+        input: " ",
+        expected: [""],
+    },
+    {
+        input: "  Use   regex  to make sure    words  are counted as expected               ",
+        expected: ["use", "regex", "to", "make", "sure", "words", "are", "counted", "as", "expected"]
+    }
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        const actual = cleanInput(input);
+        expect(actual).toHaveLength(expected.length);
+        for (const i in expected) {
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
