59262448b6f95c364427c01f786fe9c47e497b3d
MonasteryStudent <cs_knoll@proton.me>
2026-02-14 10:55:28 +0100

    feat: implement startREPL function

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
     return input.toLowerCase().trim().split(/\s+/);
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    }); 
+    rl.prompt();
+    rl.on("line", async (input) => {
+        const words = cleanInput(input);
+        if (words.length === 0) {
+            rl.prompt();
+            return;
+        }
+        const commandName = words[0];
+        console.log(`Your command was: ${commandName}`);
+        rl.prompt();
+    });
 }
