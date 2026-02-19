4a5c90cfe3ffbb688858fdd214525bfa0b04b851
MonasteryStudent <cs_knoll@proton.me>
2026-02-19 10:57:31 +0100

    refactor: add error handling to PokeAPI fetch request

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -4,16 +4,18 @@ export class PokeAPI {
   constructor() {}
 
   async fetchLocations(pageURL?: string | null): Promise<ShallowLocations> {
-    let url = pageURL;
-    if (!url) {
-      url = `${PokeAPI.baseURL}/location-area`;
+    const url = pageURL || `${PokeAPI.baseURL}/location-area`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status} ${response.statusText}`);
+      }
+      const locations: ShallowLocations = await response.json();
+      return locations;
+    } catch (e) {
+      throw new Error(`Error fetching locations: ${(e as Error).message}`)
     }
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const data: ShallowLocations = await response.json();
-    return data;
   }
 
   // async fetchLocation(locationName: string): Promise<Location> {
