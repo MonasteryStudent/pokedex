f5bdbeb3a1d5d4ba2796ca34a9a8504084a48385
MonasteryStudent <cs_knoll@proton.me>
2026-02-19 11:00:17 +0100

    refactor: add commandMapBack to command_map.ts and delete command_mapb.ts file

--- src/command_map.ts
+++ src/command_map.ts
@@ -13,3 +13,17 @@ export async function commandMap(state: State): Promise<void> {
         console.log(result.name);
     }
 }
+
+export async function commandMapBack(state: State): Promise<void> {
+    const url = state.prevLocationsURL;
+    if (!url) {
+        console.log("you're on the frist page");
+        return;
+    }
+    const locations = await state.pokeapi.fetchLocations(url);
+    state.nextLocationsURL = locations.next;
+    state.prevLocationsURL = locations.previous;
+    for (const location of locations.results) {
+        console.log(location.name);
+    }
+}
--- src/commands.ts
+++ src/commands.ts
@@ -1,8 +1,8 @@
 import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
-import { commandMap } from "./command_map.js";
-import { commandMapB } from "./command_mapb.js";
-import { CLICommand } from "./state.js";
+import { commandMap, commandMapBack } from "./command_map.js";
+
+import type { CLICommand } from "./state.js";
 
 export function getCommands(): Record<string, CLICommand> {
     return {
@@ -18,13 +18,13 @@ export function getCommands(): Record<string, CLICommand> {
         },
         map: {
             name: "map",
-            description: "Displays area names of the next 20 locations.",
+            description: "Get the next page of locations",
             callback: commandMap,
         },
         mapb: {
             name: "mapb",
-            description: "Displays area names of the previous 20 locations.",
-            callback: commandMapB,
+            description: "Get the previous page of locations",
+            callback: commandMapBack,
         }
     };
 }
